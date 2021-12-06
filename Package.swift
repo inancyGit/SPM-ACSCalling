3d084962cd32270f4cf259c65351013b7cca3ce1
Sanath Rao <sanathr@microsoft.com>
2021-12-05 23:18:09 -0800

    Adding checksum

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -16,7 +16,8 @@ let package = Package(
         .binaryTarget(
             name: "AzureCommunicationCalling",
             dependencies: ["AzureCommunicationCommon"],
-            url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip"
+            url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip",
+            checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
         )
     ]
 )
