86f730a168c30a9a59d5615442b8cfa433a1b8bf
Sanath Rao <sanathr@microsoft.com>
2021-12-05 23:23:44 -0800

    fix error

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            dependencies: ["AzureCommunicationCommon"],
             url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip",
             checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
         )
