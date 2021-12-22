852932f2325be7ccaefda93f705a81339616f608
Sanath Rao <sanathr@microsoft.com>
2021-12-22 00:36:35 -0800

    Trying another way

--- Package.swift
+++ Package.swift
@@ -9,16 +9,14 @@ let package = Package(
     products: [
         .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCallingWrapper"])
     ],
+    dependencies: [
+            .package(name: "AzureCommunicationCommon", url: "https://github.com/Azure/SwiftPM-AzureCommunicationCommon.git", from: "1.0.2")
+    ],
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
             url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip",
             checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
-        ),
-        .target(
-            name: "AzureCommunicationCommon",
-            url: "https://github.com/Azure/SwiftPM-AzureCommunicationCommon.git",
-            from: "1.0.2"
         ),
         .target(
             name: "AzureCommunicationCallingWrapper",
