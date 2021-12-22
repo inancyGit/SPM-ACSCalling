d192c884358699644d14adb707a05c237b23f37c
Sanath Rao <sanathr@microsoft.com>
2021-12-22 00:18:28 -0800

    ACS Calling Swift PM Support

--- Package.swift
+++ Package.swift
@@ -7,17 +7,28 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCalling"])
-    ],
-    dependencies: [
-        .package(name: "AzureCommunicationCommon", url: "https://github.com/Azure/SwiftPM-AzureCommunicationCommon.git", from: "1.0.2")
+        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCallingWrapper"])
     ],
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
             url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip",
             checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
+        ),
+        .target(
+            name: "AzureCommunicationCommon",
+            url: "https://github.com/Azure/SwiftPM-AzureCommunicationCommon.git",
+            from: "1.0.2"
+        ),
+        .target(
+            name: "AzureCommunicationCallingWrapper",
+            dependencies: [
+                .target(name: "AzureCommunicationCalling", condition: .when(platforms: .some([.iOS]))),
+                .target(name: "AzureCommunicationCommon",  condition: .when(platforms: .some([.iOS])))
+            ],
+            path: "AzureCommunicationCallingWrapper"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
 
