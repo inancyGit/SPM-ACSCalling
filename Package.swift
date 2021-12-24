e1ec5962a1b76113a88934a66d20d6999a9cc153
Sanath Rao <sanathr@microsoft.com>
2021-12-24 00:42:12 -0800

    move back to binary zip with embedded common

--- Package.swift
+++ Package.swift
@@ -1,32 +1,19 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.3.0
 import PackageDescription
 
 let package = Package(
     name: "AzureCommunicationCalling",
     platforms: [
-        .macOS(.v11), .iOS(.v12)
+        .iOS(.v12)
     ],
     products: [
-        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCallingWrapper"])
-    ],
-    dependencies: [
-            .package(name: "AzureCommunicationCommon", url: "https://github.com/raosanat/SwiftPM-AzureCommunicationCommon.git", from: "1.0.2")
+        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCalling"])
     ],
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            url: "https://github.com/Azure/communication/releases/download/v2.2.0-beta.1/AzureCommunicationCalling-2.2.0-beta.1.zip",
-            checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
-        ),
-        .target(
-            name: "AzureCommunicationCallingWrapper",
-            dependencies: [
-                .target(name: "AzureCommunicationCalling"),
-                .product(name: "AzureCommunicationCommon", package: "AzureCommunicationCommon")
-            ],
-            path: "AzureCommunicationCallingWrapper"
+            url: "https://github.com/raosanat/Communication/releases/download/v2.2.1-alpha.1/AzureCommunicationCalling-2.2.1-alpha.1.zip",
+            checksum: "105012f6aa8a44edd8e91027198fe778c6bb1feee7052e14b8a7f0ccb7e36c3c"
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
-
