dfd924038486d215031d9eaaea0d9188954e0909
Sanath Rao <sanathr@microsoft.com>
2021-12-27 21:47:36 -0800

    Try with no swift header file import

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            url: "https://github.com/raosanat/Communication/releases/download/v2.2.1-alpha.2/AzureCommunicationCalling-2.2.1-alpha.2.zip",
-            checksum: "865d45d08780bc118afd73b7eca4f8a03a60241f913f9023f70020220c32f85b"
+            url: "https://github.com/raosanat/Communication/releases/download/v2.2.1-alpha.4/AzureCommunicationCalling-2.2.1-alpha.4.zip",
+            checksum: "dfb1581c3425be64302d3a3b698b819f9135f9821493f7170e3b8274c14fd79b"
         )
     ]
 )
