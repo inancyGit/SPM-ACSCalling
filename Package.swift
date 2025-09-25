e13a507fc30cd97ab90c94ae5b06d15741685f4d
nancyP <pandyanancy@microsoft.com>
2025-09-25 17:35:34 -0400

    Modified Package.swift
    Removed Wrapper project

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-    products: [
-        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCallingWrapper"])
-    ],
     dependencies: [ // to use a package from SPMCommon (common sdk)
             .package(name: "SPMCommon", url: "https://github.com/inancyGit/SPMCommon.git", from: "1.0.0")
     ],
@@ -18,14 +15,6 @@ let package = Package(
             url: "https://github.com/Azure/Communication/releases/download/v2.16.0/AzureCommunicationCalling-2.16.0.zip",
             checksum: "30706c67938ee54d788aac1f66f4278ff53defb0beea6c9fc7c7567e2027af61"
         ),
-        .target(
-            name: "AzureCommunicationCallingWrapper",
-            dependencies: [
-                .target(name: "AzureCommunicationCalling"),
-                .product(name: "AzureCommunicationCommon", package: "SPMCommon")
-            ],
-            path: "AzureCommunicationCallingWrapper"
-        )
     ],
     swiftLanguageVersions: [.v5]
 )
