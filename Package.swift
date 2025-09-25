34c7fbbeb1aed3e6746f254c19f897872fdb432d
nancyP <pandyanancy@microsoft.com>
2025-09-25 16:02:31 -0400

    Modify package.swift file
    Changed the Package.swift file version name and changed the checksum

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
     products: [
         .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCallingWrapper"])
     ],
-    dependencies: [
+    dependencies: [ // to use a package from SPMCommon (common sdk)
             .package(name: "SPMCommon", url: "https://github.com/inancyGit/SPMCommon.git", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
             url: "https://github.com/Azure/Communication/releases/download/v2.16.0/AzureCommunicationCalling-2.16.0.zip",
-            checksum: "3dfc0b9e1ae79e1d8df622f39b1c85db8998518d2c02eb1fe6b9ae58333c6de9"
+            checksum: "30706c67938ee54d788aac1f66f4278ff53defb0beea6c9fc7c7567e2027af61"
         ),
         .target(
             name: "AzureCommunicationCallingWrapper",
