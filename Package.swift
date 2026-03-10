7275940a26994ed58f2c0e2138eff033e64e7702
nancyP <inancyp@outlook.com>
2026-03-09 22:45:21 -0400

    reversed

--- Package.swift
+++ Package.swift
@@ -1,33 +1,38 @@
 // swift-tools-version:6.0
-
 import PackageDescription
+
 let package = Package(
     name: "AzureCommunicationCalling",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
-        // Expose Calling as its own product
+        // This is the product your app will import
         .library(
             name: "AzureCommunicationCalling",
-            targets: ["AzureCommunicationCalling"]
-        ),
-        // Expose Common as its own product
-        .library(
-            name: "AzureCommunicationCommon",
-            targets: ["AzureCommunicationCommon"]
+            targets: ["AzureCommunicationCallingWrapper"]
         )
     ],
+    dependencies: [
+        // Git dependency
+        .package(url: "https://github.com/inancyGit/ACSCommon.git", branch: "main")
+    ],
     targets: [
+        // 1) Binary target for the .zip
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            url: "https://github.com/inancyGit/SPM-ACSCalling/releases/download/2.0.0/AzureCommunicationCalling-2.18.2-beta.2.zip",
-            checksum: "96b0faec320b9d6f3730826ad4d7de5933a275a2cbd27cfa27d6913003ea5c85"
+            url: "https://github.com/Azure/Communication/releases/download/v2.16.0/AzureCommunicationCalling-2.16.0.zip",
+            checksum: "30706c67938ee54d788aac1f66f4278ff53defb0beea6c9fc7c7567e2027af61"
         ),
-        .binaryTarget(
-            name: "AzureCommunicationCommon",
-            url:"https://github.com/Azure/azure-sdk-for-ios/releases/download/AzureCommunicationCommon_1.3.3/AzureCommunicationCommon_1.3.3.xcframework.zip",
-            checksum: "4694c77d1ef30178197c458195474b78b4e28098c821e0392c420cf5f0762568"
+        // 2) Buildable Swift target that depends on the binary + ACSCommon
+        .target(
+            name: "AzureCommunicationCallingWrapper",
+            dependencies: [
+                "AzureCommunicationCalling",
+                .product(name: "AzureCommunicationCommon", package: "ACSCommon")
+            ],
+            path: "Source/AzureCommunicationCallingWrapper"
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
